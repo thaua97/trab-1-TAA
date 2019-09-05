fa9f6d51b97c8b54717a2a2a3bc658920c500d59
Thauã Borges <thaua97@gmail.com>
2019-09-05 18:03:48 -0300

    Updates

---
+++ precache-manifest.75e842b6c84b9f2a49dd9789ed780a24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02050ad80f7ab19c2324891fec55ed21",
+    "url": "/trab-1-TAA/index.html"
+  },
+  {
+    "revision": "d830cc8f2884975bfc40",
+    "url": "/trab-1-TAA/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "d830cc8f2884975bfc40",
+    "url": "/trab-1-TAA/static/js/2.507c5dd4.chunk.js"
+  },
+  {
+    "revision": "54e37a026ebde0074a86",
+    "url": "/trab-1-TAA/static/js/main.62485cdd.chunk.js"
+  },
+  {
+    "revision": "579e27da7199979da231",
+    "url": "/trab-1-TAA/static/js/runtime~main.e4acb231.js"
+  },
+  {
+    "revision": "0c179d941ee01208a05dcdc34025379a",
+    "url": "/trab-1-TAA/static/media/vehicle.0c179d94.svg"
+  }
+]);
